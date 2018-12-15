6b3015d40d820efbccd6f0481721fe73f22de287
Bernardo Gadelha <bernardompg@gmail.com>
2018-12-15 19:11:37 -0200

    Testes de câmera

---
+++ Projeto Piloto/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public float cameraDistZ = 20.0f;
+    public float cameraDistY = 20.0f;
+    public float cameraDistX = 20.0f;
+
+    private void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.z += cameraDistZ;
+        pos.y += cameraDistY;
+        pos.x += cameraDistX;
+        transform.position = pos;
+    }
+}
