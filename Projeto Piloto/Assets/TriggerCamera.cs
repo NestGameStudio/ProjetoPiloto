94cb8b06a477810e9515692c3c56397bd5c38f52
Bernardo Gadelha <bernardompg@gmail.com>
2018-12-18 06:01:31 -0200

    asAS

---
+++ Projeto Piloto/Assets/TriggerCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerCamera : MonoBehaviour
+{
+    public Animator animacaoTorre;
+    public Animator animacaoPlayer;
+    public GameObject flowchartFinal;
+    public GameObject mainCamera;
+    public GameObject mainPlayerCamera;
+    public GameObject cutsceneCam;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //mainCamera.SetActive(false);
+            //mainPlayerCamera.SetActive(false);
+            //cutsceneCam.SetActive(true);
+
+            //animacaoTorre.gameObject.SetActive(true);
+            animacaoPlayer.gameObject.SetActive(true);            
+
+            //animacaoTorre.Play("animacaoFinal");
+            //animacaoPlayer.Play("animacaoFinalPlayer");
+
+            flowchartFinal.SetActive(true);
+
+        }
+    }
+    public void toMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
+
