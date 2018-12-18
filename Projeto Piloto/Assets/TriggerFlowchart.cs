48e3f1f452c4beb99600a85eb6b88a19eb6ef6c4
Bernardo Gadelha <bernardompg@gmail.com>
2018-12-18 02:52:06 -0200

    Cutscene + Mini Tutorial

---
+++ Projeto Piloto/Assets/TriggerFlowchart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFlowchart : MonoBehaviour
+{
+    public GameObject flowchart2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            flowchart2.SetActive(true);
+        }
+    }
+}
+
