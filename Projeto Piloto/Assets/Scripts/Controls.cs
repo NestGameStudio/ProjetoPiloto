4a69594cf238d18868dc0f3d8ad095cedeb622d0
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2018-12-27 23:52:10 -0200

    Movimentação tá perfeitinha, faltaria só balancear ela novamente e tem o bugzinho de ela não fazer o delay quando faz a rotação enquanto está andando

--- Projeto Piloto/Assets/Scripts/Controls.cs
+++ Projeto Piloto/Assets/Scripts/Controls.cs
@@ -1,20 +1,136 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Controls : MonoBehaviour {     private GameObject currentState;      private OutOfBodyExperience transition;      private bool left = true;     private bool right = true;     private bool up = true;     private bool down = true;      private float currentTime = 0;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    void Start()     {         transition = OutOfBodyExperience.Instance;          currentState = transition.body;         
+public class Controls: MonoBehaviour
+{
+    private GameObject currentState;
+
+    private OutOfBodyExperience transition;
+
+    private bool left = true;
+    private bool right = true;
+    private bool up = true;
+    private bool down = true;
+
+    private float currentTime = 0;
+
+    void Start()
+    {
+        transition = OutOfBodyExperience.Instance;
+
+        currentState = transition.body;
+
     }
 
-void LateUpdate()     {         // Movimentação         var rotation = Time.deltaTime * currentState.GetComponent<Properties>().RotationVelocity;         var x = Input.GetAxis("Horizontal") * Time.deltaTime * currentState.GetComponent<Properties>().WalkVelocity;         var y = Input.GetAxis("Vertical") * Time.deltaTime * currentState.GetComponent<Properties>().WalkVelocity;          if (Input.GetAxis("Horizontal") > 0)         { 
+    void FixedUpdate()
+    {
+        // Movimentação
+        float rotation = Time.deltaTime * currentState.GetComponent<Properties>().RotationVelocity;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * currentState.GetComponent<Properties>().WalkVelocity;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * currentState.GetComponent<Properties>().WalkVelocity;
 
-            if (left)
-            {
-                currentTime = 0;                 left = false;
-                right = true;
+        Quaternion direction = Quaternion.Euler(0, 0, 0);
+        float translation = 0;
+
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+
+            if (Input.GetAxis("Horizontal") > 0)
+            {   // Right
+
+                // Orienta a rotação para a direção certa
+                if (left)
+                {
+                    currentTime = 0;
+                    left = false;
+                    right = true;
+                }
+
+                direction = Quaternion.Euler(-90, 0, 270);
+                translation = -x;
+
+            } else if (Input.GetAxis("Horizontal") < 0)
+            {   // Left
+
+                // Orienta a rotação para a direção certa
+                if (right)
+                {
+                    currentTime = 0;
+                    left = true;
+                    right = false;
+                }
+
+                direction = Quaternion.Euler(-90, 0, 90);
+                translation = x;
             }
-             if (currentTime < 1)                 currentTime += Time.deltaTime; 
-            currentState.transform.rotation = Quaternion.SlerpUnclamped(currentState.transform.rotation, Quaternion.Euler(-90, 0, 270), currentTime);             currentState.transform.Translate(0, -x, 0);              //Debug.Log("RIGHT");         }         else if (Input.GetAxis("Horizontal") < 0)         {             if (right)             {                 currentTime = 0;                 left = true;                 right = false;             }
 
-            if (currentTime < 1)                 currentTime += Time.deltaTime;              //targetAngle = targetAngle - 90;              currentState.transform.rotation = Quaternion.SlerpUnclamped(currentState.transform.rotation, Quaternion.Euler(-90, 0, 90), currentTime);             currentState.transform.Translate(0, x, 0);             //currentState.transform.Rotate(new Vector3(0,0,90) * Time.deltaTime);              //float angle = Mathf.LerpAngle(90, 0, (Time.time - startTime) / 2);
-            //currentState.transform.eulerAngles = new Vector3(-90, 0, angle);             //Debug.Log("LEFT");         }
+            if (Input.GetAxis("Vertical") > 0)
+            {    // Up
 
-        if (Input.GetAxis("Vertical") > 0)         {             if (down)             {                 currentTime = 0;                 up = true;                 down = false;             }              if (currentTime < 1)                 currentTime += Time.deltaTime;               currentState.transform.rotation = Quaternion.SlerpUnclamped(currentState.transform.rotation, Quaternion.Euler(-90, 0, 180), currentTime);             currentState.transform.Translate(0, -y, 0);              //Debug.Log("UP");         }         else if (Input.GetAxis("Vertical") < 0)         {
-            if (up)             {                 currentTime = 0;                 up = false;                 down = true;             }              if (currentTime < 1)                 currentTime += Time.deltaTime;              currentState.transform.rotation = Quaternion.SlerpUnclamped(currentState.transform.rotation, Quaternion.Euler(-90, 0, 0), currentTime);             currentState.transform.Translate(0, y, 0);              //Debug.Log("DOWN");         }          // Mecânica do espirito         if (Input.GetKeyDown(KeyCode.F))         {             if(!transition.IsInSpiritState())             {                 transition.ReleaseSpirit();                 currentState = transition.spirit;              } else {                  transition.RetrieveSpirit();                 currentState = transition.body;             }         }     } }  
+                // Orienta a rotação para a direção certa
+                if (down)
+                {
+                    currentTime = 0;
+                    up = true;
+                    down = false;
+                }
+
+                direction = Quaternion.Euler(-90, 0, 180);
+                translation = -y;
+
+            } else if (Input.GetAxis("Vertical") < 0)
+            {     // Down
+
+                // Orienta a rotação para a direção certa
+                if (up)
+                {
+                    currentTime = 0;
+                    up = false;
+                    down = true;
+                }
+
+                direction = Quaternion.Euler(-90, 0, 0);
+                translation = y;
+            }
+
+            if (direction != Quaternion.Euler(0, 0, 0))
+            {
+                // Rotaciona até o limite do eixo
+                if (currentTime < 1)
+                    currentTime += Time.deltaTime;
+
+                //currentState.transform.Rotate(direction.ToEuler(), Space.Self);
+                currentState.transform.rotation = Quaternion.SlerpUnclamped(currentState.transform.rotation, direction, currentTime);
+
+                // Faz a translação do personagem
+                if (currentState.transform.rotation.z - direction.z <= 1)
+                {
+                    currentState.transform.Translate(0, translation, 0);
+                    //Debug.Log("Inicia movimentação");
+                }
+            }
+
+            /*float diff = player.transform.rotation.eulerAngles.y - targetRotation.eulerAngles.y;
+            float dergee = 1;
+            if (Mathf.Abs(diff) <= dergee) { Debug.Log("ready"); }*/
+        }
+
+        // Mecânica do espirito
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!transition.IsInSpiritState())
+            {
+                transition.ReleaseSpirit();
+                currentState = transition.spirit;
+
+            } else
+            {
+
+                transition.RetrieveSpirit();
+                currentState = transition.body;
+            }
+        }
+    }
+}
+
---
+++ Projeto Piloto/Assets/Scripts/Player/MoveControl.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class MoveControl: MonoBehaviour
+{ 
+    [Header ("Movimentation Variables")]
+    public float WalkVelocity = 1.5f;
+    public float RotationVelocitySlower = 5;
+
+    private GameObject objectAxisSupport;
+    private Rigidbody objectRigidBody;
+    private float rotationTime = 0;
+
+
+    void Start()
+    {
+        // Esse Objeto pai criado para ser suporte na movimentação do personagem serve para que ele consiga rotacionar livremente
+        // o objeto permite servir de base para que as direções estejam corretas nos angulos de rotação e translação
+        GameObject temporarySupport = new GameObject("Temporary Game Objects");
+        objectAxisSupport = new GameObject("Object Translation Support");
+        objectAxisSupport.transform.position = this.transform.position;
+        objectAxisSupport.transform.rotation = Quaternion.Euler(new Vector3(0,180,0));
+        objectAxisSupport.transform.parent = temporarySupport.transform;
+
+        // Realiza a movimentação em com do RigidBody pois facilita a collisão com outros objetos
+        objectRigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Esperar o jogador rotacionar até certo ponto para fazer sua translação
+        Rotation();
+        if (rotationTime > 0.15f) {
+            Translation();
+        }
+    }
+
+    // Função responsável pelo deslocamento do personagem
+    private void Translation()
+    {
+        // Calcula o deslocamento de acordo com o input horizontal e vertical
+        // Multiplica ele pela velocidade ao longo do tempo (tempo é uma constante 0,1)
+        // Normaliza o vetor de deslocamento para que a movimentação diagonal não fique mais rápida
+        Vector3 displacement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * WalkVelocity * Time.deltaTime*5;
+
+        // Calcula a posição no qual o objeto deve estar após movimentação
+        // Transforma a direção do deslocamento do objecto com base nos eixos locais definidos no objecto suporte e os transforma em coordenadas globais 
+        Vector3 newPosition = objectRigidBody.transform.position + objectAxisSupport.transform.TransformDirection(displacement);
+
+        // Move o objeto para a nova posição
+        objectRigidBody.MovePosition(newPosition);
+    }
+
+    // Função responsável pela rotação do personagem
+    private void Rotation() {
+
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+
+            // Aumenta lentamente o valor até 1 para que a rotação ocorra lentamente até que se chegue na direção desejada
+            if (rotationTime < 1) {
+                rotationTime += Time.deltaTime/RotationVelocitySlower;
+            }
+
+            // Calcula a direção que o objeto deve se rotacionar
+            Vector3 directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            // Pega o angulo com seu respectivo sinal a partir da posição do objeto e a direção desejada
+            // O vetor de posição do objeto está normalizado se resumindo ao seu vetor canônico (0s e 1s)
+            // O vetor da direção desejada está transformado em coordenadas globais e apoiados com referência ao objeto suporte
+            float angle = Vector3.SignedAngle(transform.position.normalized, objectAxisSupport.transform.TransformDirection(directionVector), Vector3.up);
+
+            // Direção do vetor em Quaternions
+            Quaternion direction = Quaternion.Euler(0, angle, 0);
+
+            // Rotaciona o objeto na direção desejada
+            objectRigidBody.MoveRotation(Quaternion.SlerpUnclamped(transform.rotation, direction, rotationTime));
+             
+        } else {
+            
+            // Se não há inputs ele zera a velocidade de rotação preparando para uma nova direção
+            if (rotationTime != 0) {
+                rotationTime = 0;
+            }
+        }
+
+    }
+}
