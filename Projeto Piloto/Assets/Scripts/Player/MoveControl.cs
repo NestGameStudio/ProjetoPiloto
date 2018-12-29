ab74ce1213e05e8b8be0f5f716b54b110d272de0
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2018-12-29 14:36:33 -0200

    Direção e rotção funcionando direitinho

--- Projeto Piloto/Assets/Scripts/Player/MoveControl.cs
+++ Projeto Piloto/Assets/Scripts/Player/MoveControl.cs
@@ -4,11 +4,17 @@ public class MoveControl: MonoBehaviour
 { 
     [Header ("Movimentation Variables")]
     public float WalkVelocity = 1.5f;
-    public float RotationVelocitySlower = 5;
+    public float RotationSlower = 5;
+
+    public float ForwardAngle = 180;
 
     private GameObject objectAxisSupport;
     private Rigidbody objectRigidBody;
     private float rotationTime = 0;
+
+    private Vector3 translationPosition;
+
+    private Vector3 previousDirection;
 
 
     void Start()
@@ -18,11 +24,14 @@ public class MoveControl: MonoBehaviour
         GameObject temporarySupport = new GameObject("Temporary Game Objects");
         objectAxisSupport = new GameObject("Object Translation Support");
         objectAxisSupport.transform.position = this.transform.position;
-        objectAxisSupport.transform.rotation = Quaternion.Euler(new Vector3(0,180,0));
+        objectAxisSupport.transform.rotation = Quaternion.Euler(new Vector3(0, ForwardAngle, 0));
         objectAxisSupport.transform.parent = temporarySupport.transform;
 
         // Realiza a movimentação em com do RigidBody pois facilita a collisão com outros objetos
         objectRigidBody = this.GetComponent<Rigidbody>();
+
+        Vector3 initialRotation = new Vector3(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z);
+        previousDirection = objectAxisSupport.transform.TransformDirection(initialRotation);
     }
 
     void FixedUpdate()
@@ -44,25 +53,33 @@ public class MoveControl: MonoBehaviour
 
         // Calcula a posição no qual o objeto deve estar após movimentação
         // Transforma a direção do deslocamento do objecto com base nos eixos locais definidos no objecto suporte e os transforma em coordenadas globais 
-        Vector3 newPosition = objectRigidBody.transform.position + objectAxisSupport.transform.TransformDirection(displacement);
+        translationPosition = objectRigidBody.transform.position + objectAxisSupport.transform.TransformDirection(displacement);
 
         // Move o objeto para a nova posição
-        objectRigidBody.MovePosition(newPosition);
+        objectRigidBody.MovePosition(translationPosition);
     }
 
     // Função responsável pela rotação do personagem
     private void Rotation() {
 
-
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
 
-            // Aumenta lentamente o valor até 1 para que a rotação ocorra lentamente até que se chegue na direção desejada
+            // Aumenta lentamente o valor até 1 para que a rotação ocorra lentamente até que se chegue na direção desejada e não continue mais
             if (rotationTime < 1) {
-                rotationTime += Time.deltaTime/RotationVelocitySlower;
+                rotationTime += Time.deltaTime / RotationSlower;
+
+                if (rotationTime > 1) {
+                    rotationTime = 1;
+                }
             }
 
             // Calcula a direção que o objeto deve se rotacionar
             Vector3 directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            /*if (previousDirection != directionVector) {
+                Debug.Log("Change Direction");
+                previousDirection = directionVector;
+            }*/
 
             // Pega o angulo com seu respectivo sinal a partir da posição do objeto e a direção desejada
             // O vetor de posição do objeto está normalizado se resumindo ao seu vetor canônico (0s e 1s)
@@ -73,8 +90,8 @@ public class MoveControl: MonoBehaviour
             Quaternion direction = Quaternion.Euler(0, angle, 0);
 
             // Rotaciona o objeto na direção desejada
-            objectRigidBody.MoveRotation(Quaternion.SlerpUnclamped(transform.rotation, direction, rotationTime));
-             
+            objectRigidBody.MoveRotation(Quaternion.SlerpUnclamped(transform.rotation, direction, rotationTime));  
+
         } else {
             
             // Se não há inputs ele zera a velocidade de rotação preparando para uma nova direção
